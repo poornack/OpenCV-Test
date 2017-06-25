bf71766c23f2b5270e3e0ba017686ad35809395f
Poorna Chander Kalidas <Poorna@Poornas-MacBook-Pro.local>
2017-06-24 18:58:20 -0700

    Initial Commit

---
+++ OpenCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenCV
+//
+//  Created by Poorna Chander Kalidas on 2017-06-24.
+//  Copyright © 2017 Poorna Chander Kalidas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
