9e2d29ca41fe45c4032adeaf32fa0ef895a203c3
Poorna Chander Kalidas <Poorna@Poornas-MacBook-Pro.local>
2017-06-25 01:59:02 -0700

    Convert colour image to grey. Read image properties.

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -12,24 +12,55 @@
 #include "opencv2/highgui.hpp"
 #include "opencv2/opencv.hpp"
 
+
+
 int main(int argc, const char * argv[]) {
     
-    cv::Mat image;
-    cv::VideoCapture camera(0);
+    cv::Mat imageUnchanged = cv::imread("../../Pictures/honda_civic.jpeg", CV_LOAD_IMAGE_UNCHANGED);
     
-    cv::namedWindow("Poorna Face", cv::WINDOW_AUTOSIZE);
+    imshow("unchanged", imageUnchanged);
     
-    camera >> image;
+    std::cout << "Image row count: " << imageUnchanged.rows << std::endl;
+    std::cout << "Image coloumn count: " << imageUnchanged.cols << std::endl;
+    std::cout << "Image colour depth: " << imageUnchanged.channels() << std::endl;
     
-    if(!image.empty()) {
-        cv::imshow("Poorna Face", image);
-    }
+    cv::Mat imageGray;
+    
+    cv::cvtColor(imageUnchanged, imageGray, CV_BGR2GRAY);
+    
+    imshow("gray", imageGray);
+    
+    std::cout << "Image row count: " << imageGray.rows << std::endl;
+    std::cout << "Image coloumn count: " << imageGray.cols << std::endl;
+    std::cout << "Image colour depth: " << imageGray.channels() << std::endl;
+    
+    //imshow("grayscale", imageMono);
     
     cv::waitKey(0);
     
-    cv::destroyAllWindows();
-    
-    camera.release();
     
     return 0;
 }
+
+
+/*
+Read image from file:
+    cv::imread("/filename", FLAG); FLAGS: CV_LOAD_IMAGE_UNCHANGED, CV_LOAD_IMAGE_GRAYSCALE, ...
+ 
+crop:
+    imshow("cropped", imageUnchanged(cv::Rect(100,100,100,100))); //rect(x, y, lenght, width)
+    cv::Rect r(10, 10, 100, 100)
+    cv::Mat imageCropped = imageUnchanged(r);
+ 
+convert colour:
+    cv::cvtColor(originalImage, newImage, FLAG); FLAGS: CV_BGR2GRAY, ...
+ 
+Read image characteristics:
+     std::cout << "Image row count: " << imageGray.rows << std::endl;
+     std::cout << "Image coloumn count: " << imageGray.cols << std::endl;
+     std::cout << "Image colour depth: " << imageGray.channels() << std::endl;
+
+
+
+
+*/
